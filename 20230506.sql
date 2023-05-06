ad38000f32c3f20aae3f92666f342a9dfec30475
blacktea890329 <108226946+blacktea890329@users.noreply.github.com>
2023-05-06 17:12:36 +0800

    Add files via upload

---
+++ 20230506.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2023-05-06 11:12:18
+-- 伺服器版本： 10.4.24-MariaDB
+-- PHP 版本： 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `test`
+--
+CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `test`;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `data`
+--
+
+CREATE TABLE `data` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(10) NOT NULL,
+  `sex` int(1) NOT NULL,
+  `cratetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 傾印資料表的資料 `data`
+--
+
+INSERT INTO `data` (`id`, `user_name`, `sex`, `cratetime`) VALUES
+(19, 'asdsadasda', 1, '2023-05-06 15:51:37'),
+(20, 'asdsadqwwe', 0, '2023-05-06 16:27:32'),
+(22, 'qwfqf', 1, '2023-05-06 16:27:41'),
+(23, 'qsdf', 1, '2023-05-06 16:27:46');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `data`
+--
+ALTER TABLE `data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
