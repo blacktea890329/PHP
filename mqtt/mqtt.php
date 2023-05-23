7c65fa81f974d30c4c54146c154e9d5ef4fd40a3
blacktea890329 <108226946+blacktea890329@users.noreply.github.com>
2023-05-23 09:08:24 +0800

    按鈕觸發PHP發送mqtt
    按鈕觸發PHP發送mqtt

--- mqtt/mqtt.php
+++ mqtt/mqtt.php
@@ -1,61 +1,20 @@
 <?php
-	//引入MQTT客戶端庫
-	require("phpMQTT.php");
-	
-	// MQTT設置
-	$server = 'localhost';
-	$port = 1883;
-	$username = '';
-	$password = '';
-	$clinet_id = 'phpMQTT-subscriber';
-	$mqtt = new Bluerhinos\phpMQTT($server, $port, $clinet_id);
-	
-	// 連接到MQTT
-	if ($mqtt->connect()){
-		
-		//訂閱主題
-		$topics['iot/device'] = array("qos"=> 0, "function" => "processMessage");
-		$mqtt->subscribe($topics, 0);
-		
-		//持續監聽MQTT訊息
-		while ($mqtt->proc()){
-			
-			
-		}
-		
-		//斷開MQTT連接
-		$mqtt->close();
-	
-	} else {
-		
-		echo "Failed to connect to MQTT broker";
-		
-	}
-	
-		//處理接收到的訊息
-		function processMessage($topic, $message) {
-			
-			// 解析收到的訊息
-			$data = json_decode($message, true);
-			
-			// 提取需要的數據
-			$sensor = $data['sensorname'];
-			$senorValue = $data['sensorValue'];
-			// 將數據寫入資料庫
-			$conn = new mysqli("localhost","root","","mqtt");
-			
-			if ($conn->connect_error) {
-				die("資料庫連接失敗:".$conn->connect_error);
-			}
-			
-			$sql = "INSERT INTO mqtt (name,value) VALUES ('$sensor','$senorValue')";
-			
-			if ($conn->query($sql) === TRUE){
-				echo "訊息已成功寫入資料庫";
-			}
-			else{
-					echo "寫入資料庫時發生錯誤:" . $conn->error;
-			}
-			$conn->close();
-		}
-?>
+
+require('phpMQTT.php');
+
+$server = 'mqttgo.io';     // broker位置
+$port = 1883;                     // broker的port
+$username = '';                   // set your username
+$password = '';                   // set your password
+$client_id = 'phpMQTT-publisher'; // make sure this is unique for connecting to sever - you could use uniqid()
+
+$mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
+
+if ($mqtt->connect(true, NULL, $username, $password)) {
+	$mqtt->publish('black', 'Hello World! at ' . date('r'), 0, false);//(topic , 要發送的訊息)
+	$mqtt->close();
+	echo"<script>alert('發送成功');</script>";
+	echo"<script>location.href='new.php'</script>";
+} else {
+    echo "Time out!\n";
+}
